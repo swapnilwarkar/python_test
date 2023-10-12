a303177ee554755710a191c1029e928dd8d9c841
warkargs <swapnil.warkar@gslab.com>
2023-10-12 20:54:50 +0530

    checkin the features - git book and python scripts

---
+++ python_class_fstring/fstring.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+import sys
+import logging
+import os
+
+
+
+if __name__ == '__main__':
+    name = 'Swapnil';
+    age  = 24;
+    sentence = f'{name} is {age} years old';
+    print(sentence);
+    print(f'{name} is {age} years old')
