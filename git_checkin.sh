45a118baf43880cdc8fea692b7ae8bdc276d9cc9
warkargs <swapnil.warkar@gslab.com>
2023-10-13 16:55:36 +0530

    checkin the features - oops questions docs

--- git_checkin.sh
+++ git_checkin.sh
@@ -1,7 +1,7 @@
 git branch
 git status 
 git add . 
-git commit -m "checkin the features - git book and python scripts" 
+git commit -m "checkin the features - oops questions docs" 
 git push origin oct_release
 git status
 git log -n2
