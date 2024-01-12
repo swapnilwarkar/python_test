a9945702589c9a0af688b91899cd950336b317f3
warkargs <swapnil.warkar@gslab.com>
2024-01-12 11:44:50 +0530

    checkin the features - adding additional docs

--- git_checkin.sh
+++ git_checkin.sh
@@ -1,7 +1,7 @@
 git branch
 git status 
 git add . 
-git commit -m "checkin the features - oops questions docs" 
+git commit -m "checkin the features - adding additional docs" 
 git push origin oct_release
 git status
 git log -n2
